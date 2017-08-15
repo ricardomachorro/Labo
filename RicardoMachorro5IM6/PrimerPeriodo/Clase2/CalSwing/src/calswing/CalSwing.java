031dab775439f8fbbce1c63cfdc9bebfc48b7609
ricardomachorro <ricardoalbeto.machorrovences@gmail.com>
2017-08-15 16:43:15 -0500

    Cambios

---
+++ RicardoMachorro5IM6/PrimerPeriodo/Clase2/CalSwing/src/calswing/CalSwing.java
@@ -0,0 +1,18 @@
+
+package calswing;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class CalSwing extends JFrame implements ActionListener {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ RicardoMachorro5IM6/PrimerPeriodo/Clase2/CalculadoraMain/src/calculadoramain/CalculadoraMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoramain;
+
+/**
+ * Ejercicio: Calculadora basica 
+ * Implementar la Interfaz de usuario 
+ * Implementar los metodos siguientes a través de los listeners adecuados: 
+ * - Sumar 
+ * - Restar 
+ * - Multiplicar 
+ * - Dividir
+ * 
+ *@author:  emmanuel 
+ * @version:  1.0 
+ * @date: 06-09-2015 
+ */
+public class CalculadoraMain {
+ public static void main(String[] args) {
+		Calculadora calculadora = new Calculadora();
+		calculadora.setVisible(true);
+	}   
+}
